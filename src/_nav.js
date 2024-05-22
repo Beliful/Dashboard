901d100b56f151b9d075674293eed950f988b154
unknown <betul.dogrul@ug.bilkent.edu.tr>
2024-05-22 15:52:50 +0300

    added dashboard

--- src/_nav.js
+++ src/_nav.js
@@ -19,6 +19,12 @@ import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 const _nav = [
   {
     component: CNavItem,
+    name: 'Dashboard Temp',
+    to: '/dashboard-template',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
     name: 'Dashboard',
     to: '/dashboard',
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
