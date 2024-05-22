2e166a2daf58e28fa311081a3b8581ab802d13d0
unknown <betul.dogrul@ug.bilkent.edu.tr>
2024-05-22 18:14:35 +0300

    iot devices are added in the map

---
+++ src/data/iotDevice.js
@@ -0,0 +1,22 @@
+const iotDevices = [
+    {
+      id: 1,
+      name: 'Sensor 1',
+      location: {
+        latitude: 40.7128,
+        longitude: -74.006,
+      },
+    },
+    {
+      id: 2,
+      name: 'Sensor 2',
+      location: {
+        latitude: 51.5074,
+        longitude: -0.1278,
+      },
+    },
+    // Add more IOT devices as needed
+  ];
+  
+  export default iotDevices;
+  
--- src/views/base/map/Map.js
+++ src/views/base/map/Map.js
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import CIcon from '@coreui/icons-react'
-import { cilTruck } from '@coreui/icons';
-import './Map.css';
-import image from './icons8-tractor-50.png'
-import tractors from 'src/data/tractor'
+import imageTruck from './icons8-tractor-50.png';
+import imageIOT from './icons8-iot-64.png'; // Assuming you have an IOT icon image
+import tractors from 'src/data/tractor'; // Assuming you have tractor data
+import iotDevices from 'src/data/iotDevice'; // Assuming you have IOT device data
 
 const iconTruck = new L.icon({
-  iconUrl: image,
-  iconSize: [30, 30], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62], // the same for the shadow
-  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  iconUrl: imageTruck,
+  iconSize: [30, 30],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+});
+
+const iconIOT = new L.icon({
+  iconUrl: imageIOT,
+  iconSize: [30, 30],
+  iconAnchor: [15, 15],
+  popupAnchor: [0, -15],
 });
 
 const Map = () => {
+  const [showTractors, setShowTractors] = useState(true); // State to toggle between tractors and IOT devices
+
+  const toggleDevices = () => {
+    setShowTractors(!showTractors);
+  };
+
+  const devices = showTractors ? tractors : iotDevices; // Determine which devices to show based on state
+
   return (
-    <MapContainer center={[39, 35]} zoom={6} scrollWheelZoom={false}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-      />
-      {tractors.map(tractor => (
-        <Marker key={tractor.id} position={[tractor.location.latitude, tractor.location.longitude]} icon={iconTruck}>
-          <Popup>
-            {tractor.plateNumber}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div style={{ height: '500px' }}> {/* Ensure the container has a height */}
+      <button onClick={toggleDevices}>{showTractors ? 'Show IOT Devices' : 'Show Tractors'}</button>
+      <MapContainer center={[39, 35]} zoom={6} scrollWheelZoom={false} style={{ height: '100%' }}> {/* Set height for the MapContainer */}
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
+        {devices.map(device => (
+          <Marker key={device.id} position={[device.location.latitude, device.location.longitude]} icon={showTractors ? iconTruck : iconIOT}>
+            <Popup>
+              {device.name}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
-}
+};
 
 export default Map;
