bb9913df0f892010e83f8076cda2eb114152063b
unknown <betul.dogrul@ug.bilkent.edu.tr>
2024-05-22 17:50:01 +0300

    added tractor icon

--- src/views/base/map/Map.js
+++ src/views/base/map/Map.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import CIcon from '@coreui/icons-react'
+import { cilTruck } from '@coreui/icons';
+import './Map.css';
+import image from './icons8-tractor-50.png'
+import tractors from 'src/data/tractor'
 
-const tractors = [
-  { id: 1, position: [39.1, 35.2], name: 'Tractor 1' },
-  { id: 2, position: [39.3, 35.4], name: 'Tractor 2' },
-  // Add more tractors as needed
-];
+const iconTruck = new L.icon({
+  iconUrl: image,
+  iconSize: [30, 30], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
 
 const Map = () => {
   return (
-    <MapContainer center={[39, 35]} zoom={6} scrollWheelZoom={false} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={[39, 35]} zoom={6} scrollWheelZoom={false}>
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
       />
       {tractors.map(tractor => (
-        <Marker key={tractor.id} position={tractor.position}>
+        <Marker key={tractor.id} position={[tractor.location.latitude, tractor.location.longitude]} icon={iconTruck}>
           <Popup>
-            {tractor.name}
+            {tractor.plateNumber}
           </Popup>
         </Marker>
       ))}
