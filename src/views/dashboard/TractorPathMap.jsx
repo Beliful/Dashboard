81d57ccd3079692c42310085c412b3cd9d9104c4
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2024-06-17 20:59:33 +0300

    chore: Updated map zoom level

--- src/views/dashboard/TractorPathMap.jsx
+++ src/views/dashboard/TractorPathMap.jsx
@@ -5,23 +5,6 @@ import 'leaflet/dist/leaflet.css'
 import { createCustomIcon } from '../../util/createLeafletIcon'
 import { LocationOn, PushPin } from '@mui/icons-material'
 
-// Define custom icons for the start and finish points
-const startIcon = new L.Icon({
-  iconUrl: 'https://example.com/start-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-})
-
-const finishIcon = new L.Icon({
-  iconUrl: 'https://example.com/finish-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-})
-
 const TractorPathMap = ({ data: tractorMeasurements }) => {
   const position =
     tractorMeasurements.length > 0
@@ -41,7 +24,7 @@ const TractorPathMap = ({ data: tractorMeasurements }) => {
   const finishPoint = polyline[polyline.length - 1]
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={position} zoom={14} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
